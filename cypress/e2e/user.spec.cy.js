404a061b2e3697ffe3f325affc0a32fab987b9c6
luizamachado-qa <luizamachado.ds@gmail.com>
2026-01-19 15:46:04 -0300

    Add page objects

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,41 +1,47 @@
 import userData from '../fixtures/users/user-date.json'
+import LoginPage from '../pages/loginPage.js'
+import DashboardPage from '../pages/dashboardPage.js'
+import MenuPage from '../pages/menuPage.js'
+
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+const menuPage = new MenuPage()
 
 describe('Orange HRM - Tests', () => {
 
   const selectorList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton: "[type='submit']",
-    sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
-    dashboardGrid: '.orangehrm-dashboard-grid',
-    wrongCredentialAlert: '.oxd-alert',
-    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
     firstNameField: "[name='firstName']",
     lastNameField: "[name='lastName']",
     genericField: ".oxd-input--active",
     dateField: "[placeholder= 'yyyy-mm-dd']",
     dateCloseButton: ".--close",
-    submitButton: "[type='submit']"
+    submitButton: "[type='submit']",
+    optionsField: ".oxd-select-text--after",
+    optionNationality: ":nth-child(27) > span",
+    optionMarital: ":nth-child(3) > span"
+  
   }
 
 
   it.only('User Info Update - Success', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorList.loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorList.dashboardGrid)
-    cy.get(selectorList.myInfoButton).click()
+    loginPage.accessLoginPage()
+    loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
+
+    dashboardPage.checkDashboardPage()
+
+    menuPage.accessMyInfo()
+
     cy.get(selectorList.firstNameField).clear().type('FirstNameTest')
     cy.get(selectorList.lastNameField).clear().type("LastNameTest")
-    cy.get(selectorList.genericField).eq(4).clear().type("123")
-    cy.get(selectorList.genericField).eq(5).clear().type("OtherIdTest")
-    cy.get(selectorList.genericField).eq(6).clear().type("DriversLicenseTest")
-    cy.get(selectorList.genericField).eq(7).clear().type("2025-03-10")
+    cy.get(selectorList.genericField).eq(3).clear().type("Employee")
+    cy.get(selectorList.genericField).eq(4).clear().type("OtherIdTest")
+    cy.get(selectorList.genericField).eq(5).clear().type("DriversLicenseTest")
+    cy.get(selectorList.genericField).eq(6).clear().type("2025-03-10")
     cy.get(selectorList.dateCloseButton).click()
-    cy.get(selectorList.genericField).eq(8).type("ssnNumber Test")
-    cy.get(selectorList.genericField).eq(9).type("sinNumber Test")
+    cy.get(selectorList.optionsField).eq(0).click()
+    cy.get(selectorList.optionNationality).click()
+    cy.get(selectorList.optionsField).eq(1).click()
+    cy.get(selectorList.optionMarital).click()
     cy.get(selectorList.submitButton).eq(0).click()
     cy.get('body').should('contain', 'Successfully Updated')
   })
